1d95c2619ac53f65cf0af7f41c1114708bf05de4
nurikhsan_x <nurikhsan_x@telkomsel.co.id>
2023-03-04 15:09:42 +0700

    remove unused

--- src/main/java/id/co/admin/desa/model/VillageOrganization.java
+++ src/main/java/id/co/admin/desa/model/VillageOrganization.java
@@ -1,13 +1,16 @@
 package id.co.admin.desa.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
