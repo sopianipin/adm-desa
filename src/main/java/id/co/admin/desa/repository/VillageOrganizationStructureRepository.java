d6cd92e0e1b5dda5d398ac585c88af4ad2643fa0
nurikhsan_x <nurikhsan_x@telkomsel.co.id>
2023-03-06 20:24:39 +0700

    add cors and delete vilage structure

--- src/main/java/id/co/admin/desa/repository/VillageOrganizationStructureRepository.java
+++
@@ -1,8 +0,0 @@
-package id.co.admin.desa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import id.co.admin.desa.model.VillageOrganizationalStructure;
-
-public interface VillageOrganizationStructureRepository extends JpaRepository<VillageOrganizationalStructure, Long> {
-}
